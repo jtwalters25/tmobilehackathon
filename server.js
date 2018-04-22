481fba25a1869b2861a5f1521b1cac33921eeb12
jtwalters25 <jeremiaht.walters@gmail.com>
2018-04-21 23:28:15 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,42 @@
+const http = require('http');
+const express = require('express');
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
+const bodyParser = require('body-parser');
+const https = require("https");
+const app = express();
+
+app.use(bodyParser());
+
+const url =
+  "https://api.thingspeak.com/channels/480554/feeds/last.json?api_key=YMKTH12K5ZN9NXM4";
+
+https.get(url, res => {
+  let body = "";
+  res.on("data", data => {
+    body += data;
+  });
+  res.on("end", () => {
+    body = JSON.parse(body);
+    console.log(body
+    );
+  });
+});
+
+app.post('/', (req, res) => {
+  const twiml = new MessagingResponse();
+
+  if (req.body.Body == 'Park') {
+    twiml.message('Hi!');
+  } else if(req.body.Body == 'bye') {
+    twiml.message('Goodbye');
+  } else {
+    twiml.message('No Body param match, Twilio sends this in the request to your server.');
+  }
+
+  res.writeHead(200, {'Content-Type': 'text/xml'});
+  res.end(twiml.toString());
+});
+
+http.createServer(app).listen(1337, () => {
+  console.log('Express server listening on port 1337');
+});
